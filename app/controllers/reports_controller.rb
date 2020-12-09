da639d01519ad12c245ad171dc7b62afb8d01fe7
miluska schaeffer <miluska.schaeffer@gmail.com>
2020-12-09 17:47:07 +0100

    implemented authenticate server method

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,17 @@
 class ReportsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_server
   def handle
-    p request.headers["X-Server-CodeName"]
-    p request.headers["X-Server-Token"]
-
     render json: { message: "test" }
   end
+
+  def authenticate_server
+    code_name = request.headers["X-Server-CodeName"]
+    server = Server.find_by(code_name: code_name)
+    access_token = request.headers["X-Server-Token"]
+    unless server && server.access_token == access_token
+      render json: { message: "Wrong Credentials" }
+    end
+  end
+
 end
