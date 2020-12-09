11026126f7056608c3d6f057bd69e2282729cf68
miluska schaeffer <miluska.schaeffer@gmail.com>
2020-12-09 19:12:35 +0100

    redefined handle, parsed csv

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,14 @@
+require 'csv'
 class ReportsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authenticate_server
   def handle
+    # need process csv file sent in the request
+    report = params[:report].open
+    csv_options = { col_sep: ',', headers: :first_row }
+    CSV.parse(report, csv_options) do |row|
+      p row
+   end
     render json: { message: "test" }
   end
 
